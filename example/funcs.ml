aac126786bdd9c2883728d70d0d66fd6dd7d57c2
Aleksander Kiryk <alkiryk@wp.pl>
2022-05-25 18:50:00 +0200

    add examples

---
+++ example/funcs.ml
@@ -0,0 +1,11 @@
+(def double-op (fn (op n)
+  (op n n)))
+
+(def twice (fn (n)
+  (double-op add n)))
+
+(def squared (fn (n)
+  (double-op mul n)))
+
+(print (twice 4) (squared 4)) ; prints '8 16'
+
