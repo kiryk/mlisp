b41ffd207c44158cc51822d0e57a90a643a36573
Aleksander Kiryk <alkiryk@wp.pl>
2022-05-25 22:25:04 +0200

    add find-primes example

---
+++ example/find-primes.ml
@@ -0,0 +1,14 @@
+(def primes (list))
+(def n 100)
+(def p 2)
+(def i 0)
+
+(while (< (len primes) n) (do
+	(set i 0)
+	(while (and (< i (len primes))
+	            (!= 0 (% p (list-field primes i))))
+	  (set i (+ i 1)))
+	(if (= i (len primes)) (do
+		(print p)
+		(set (list-field primes i) p)))
+	(set p (+ p 1))))
