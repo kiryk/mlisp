702158d1648a6fe28184d38589ce25e213d8d83e
Aleksander Kiryk <alkiryk@wp.pl>
2022-05-25 22:02:29 +0200

    add fizzbuzz example

---
+++ example/fizzbuzz.ml
@@ -0,0 +1,7 @@
+(def i 1)
+(while (<= i 100) (do
+	(print (if (= (% i 15) 0) "fizzbuzz"
+	       (if (= (% i 5)  0) "buzz"
+				 (if (= (% i 3)  0) "fizz"
+				                    i))))
+	(set i (+ i 1))))
